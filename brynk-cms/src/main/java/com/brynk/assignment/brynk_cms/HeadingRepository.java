daec4b500a3d6178ed4434daece0bf6abcc9d066
Sarithakumari-31 <sarithasamira.7@gmail.com>
2025-07-15 00:41:01 +0530

    🚀 Cleaned up structure and rebuilt JAR

--- brynk-cms/src/main/java/com/brynk/assignment/brynk_cms/HeadingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.brynk.assignment.brynk_cms;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HeadingRepository extends JpaRepository<Heading, Long> {
-    // No extra methods needed for basic CRUD
-}
